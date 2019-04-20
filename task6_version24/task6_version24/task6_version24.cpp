61629e17eb5bc4ea0a2ac8086871038038225762
mrerro <dyakov.hellvisionstudio@gmail.com>
2019-04-21 01:54:31 +0500

    add подзадача 2

--- task6_version24/task6_version24/task6_version24.cpp
+++ task6_version24/task6_version24/task6_version24.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
 #include <deque>
 #include <list>
+#include <vector>
 
-void myDequeTester() {
+void myDequeTester() { //задача 1
 	std::deque <std::list<int>> myDeque = {};
 	std::list <int> one = { 1,1 };
 	std::list <int> two = { 2 };
@@ -29,7 +30,29 @@ void myDequeTester() {
 	}
 }
 
+void thinOutTheVector() {	//задача 2 "Проредить vector tepm относительно vector values"
+	
+	std::vector <int> temp = { 1,2,3,1,42,38,5,6,5,80,60 };
+	std::vector <int> values = { 1,2,42,38 };
+	for (std::vector <int>::iterator it = temp.begin(); it != temp.end(); ++it)
+	{
+		if (std::find(values.begin(), values.end(), *it) == values.end()) {
+			if (it != temp.begin())
+				temp.erase(it--); // если элемент не первый, то итератор на предыдущий элемент остается валидным
+			else
+			{
+				temp.erase(it); // удалепние 1-го элемента
+				it = temp.begin(); // восстанавливаем итератор
+			}
+		}
+	}
+	for (int item_temp : temp) {
+		std::cout << item_temp << " ";
+	}
+}
+
 int main()
 {
-	myDequeTester();
+	//myDequeTester();	
+	thinOutTheVector();
 }
