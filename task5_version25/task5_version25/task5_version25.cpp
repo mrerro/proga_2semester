2fc14587948a6ff5572e84c441f06c9416ba9937
mrerro <dyakov.hellvisionstudio@gmail.com>
2019-04-20 14:42:09 +0500

    add task5_version25 add in fun findeInFile open file

---
+++ task5_version25/task5_version25/task5_version25.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+template <typename T>
+T findeInFile(T fileName, std::string blockName , std::string parametrName) {
+	std::fstream fs;
+	fs.open(fileName, std::fstream::in);
+	if (!fs) {
+		std::cout << "Error with file name " << fileName << " no in current directory" << std::endl;
+		return NULL;
+	}
+	fs.close();
+}
+
+
+int main()
+{
+	if (auto a = findeInFile("inpu.txt", "", "") != NULL) {
+		std::cout << a;
+	}	
+}
