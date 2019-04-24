74e9787a83f064c4a223e140eff73a2e1042df9d
mrerro <dyakov.hellvisionstudio@gmail.com>
2019-04-25 01:15:40 +0500

    Проверки на то что передан параметр в функцию проверка что файл существует

--- task5_version24/task5_version24/task5_version24.cpp
+++ task5_version24/task5_version24/task5_version24.cpp
@@ -1,4 +1,43 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <locale>
+
+
+void parseDB(std::string fileName) {
+	std::fstream fs;
+	std::string str;
+	std::string group;
+	std::list<std::string> out = {};
+	bool isBlockFound = false;
+	bool isParemetrFound = false;
+	try {
+		if (fileName.length() == 0) {
+			throw 1;
+		}
+		fs.open(fileName, std::fstream::in);
+		if (!fs) {
+			throw 2;
+		}
+	}
+	catch (int error) {
+		switch (error)
+		{
+		case 1:
+			std::cout << "Error passed empty parameter" << std::endl;
+			break;
+		case 2:
+			std::cout << "Error with file name " << fileName << " no in current directory" << std::endl;
+			break;
+		case 3:
+			std::cout << "Error there are no matching parameters in the file" << std::endl;
+			break;
+		}
+		fs.close();
+	}
+}
 
 int main()
 {
